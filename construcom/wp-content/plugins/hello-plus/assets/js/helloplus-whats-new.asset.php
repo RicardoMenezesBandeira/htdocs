257cb3b28ea2fecfa829d88464b9cdc9a0dcc89c
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-07-25 08:31:21 -0300

    helloworld-2

---
+++ construcom/wp-content/plugins/hello-plus/assets/js/helloplus-whats-new.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => '13dbf081c75c1a1774c3');
---
+++ construcom/wp-content/plugins/hello-plus/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/hello-plus',
+        'pretty_version' => '1.7.2.x-dev',
+        'version' => '1.7.2.9999999-dev',
+        'reference' => 'c20146843c0ea28845e27cda10b896bc5f913540',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/hello-plus' => array(
+            'pretty_version' => '1.7.2.x-dev',
+            'version' => '1.7.2.9999999-dev',
+            'reference' => 'c20146843c0ea28845e27cda10b896bc5f913540',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'elementor/wp-notifications-package' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => 'dd25ca9dd79402c3bb51fab112aa079702eb165e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elementor/wp-notifications-package',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
